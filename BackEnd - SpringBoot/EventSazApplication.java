9f553c27b43ebcbf65a35f4143709e97f0b7a732
Rabab-Alvi <124186350+Rabab-Alvi@users.noreply.github.com>
2025-05-13 16:16:34 +0500

    Add files via upload

---
+++ BackEnd - SpringBoot/EventSazApplication.java
@@ -0,0 +1,13 @@
+package com.RababEraj.EventSaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSazApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSazApplication.class, args);
+	}
+
+}
